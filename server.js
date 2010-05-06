3f64c9f8ad9e01a1ec9d4b8a35174fd62d27a8f8
fyorl <fyorl@backus.warwickcompsoc.co.uk>
2010-05-06 02:42:08 +0100

    Basic server functionality rewritten

--- server.js
+++ server.js
@@ -8,43 +8,80 @@ if (process.argv.length < 3) var port = 80;
 else var port = parseInt(process.argv[2]);
 
 eval(fs.readFileSync('assets/js/mootools.js'));
+eval(fs.readFileSync('assets/js/extensions.js'));
 eval(fs.readFileSync('.htaccess.js'));
 
-var Server = {
+var Server = new Class({
 	successCode: 200,
 	headers: {
 		'Content-Type': 'text/html'
 	},
 	body: '',
 	
+	chunkedOutput: function(file, mime, encoding) {
+		this.res.writeHead(this.successCode, {'Content-Type': mime});
+		var stream = fs.createReadStream(file, {'encoding': encoding});
+		
+		stream.addListener('data', function(data) {
+			if (encoding === 'binary') data = data.toString(encoding, 0, data.length);
+			this.res.write(data, encoding);
+		}.bind(this));
+		
+		stream.addListener('end', function() {
+			this.res.end();
+		}.bind(this));
+	},
+	
 	concatPath: function(path, concat) {
 		if (path.substring(path.length - 1, 1) === '/') return path + concat;
 		else return path + '/' + concat;
-	}
+	},
 	
-	handleRequest: function(req, res) {
+	initialize: function(req, res) {
 		this.req = req;
 		this.res = res;
 		
-		var path = req.url.substring(1, req.url.length - 1);
-		var stats = fs.statSync(path);
-		var file;
-		if (!stats.isFile()) {
-			if (!stats.isDirectory()) {
-				if (Redirect[path]) {
-					if (!fs.statSync(Redirect[path])) this.notFound();
-					else file = Redirect[path];
-				} else this.notFound();
-			} else file = this.concatPath(path, 'index.js');
-		} else file = path;
+		var path = req.url.substring(1, req.url.length);
+		var stats = false;
 		
+		try {
+			stats = fs.statSync(path);
+		} catch (e) {}
 		
+		var file = false;
+		
+		if (!stats) {
+			if (Redirect[path]) {
+				var redir = false;
+				try {
+					redir = fs.statSync(Redirect[path]);
+				} catch (e) {}
+				if (!redir) this.notFound();
+				else file = Redirect[path];
+			} else this.notFound();
+		} else {
+			if (stats.isFile()) file = path;
+			else if (stats.isDirectory()) file = this.concatPath(path, 'index.js');
+			else this.notFound();
+		}
+		
+		if (!file) return;
+		
+		var extension = file.split('.').getLast();
+		if (file.indexOf('.') < 0) extension = 'txt';
+		var mime = ExtensionMap[extension];
+		var encoding = 'utf8';
+		
+		if (mime.split('/')[0] === 'image') encoding = 'binary';
+		
+		if (mime === 'text/javascript') eval(fs.readFileSync(file));
+		else this.chunkedOutput(file, mime, encoding);
 	},
 	
 	notFound: function() {
 		this.successCode = 404;
 		this.writeResponse(true);
-	}
+	},
 	
 	writeResponse: function(wholeChunk) {
 		this.headers = $merge(this.headers, {
@@ -54,6 +91,8 @@ var Server = {
 		if (wholeChunk) this.res.write(this.body);
 		this.res.end();
 	}
-};
+});
 
-http.createServer(Server.handleRequest.bind(Server)).listen(port);
+http.createServer(function(req, res) {
+	new Server(req, res);
+}).listen(port);
